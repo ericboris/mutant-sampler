6ba668c3f5d0f4e634f5b152eda91a0861b24482
ericboris <ericboris@tutanota.com>
2023-06-01 17:35:55 -0700

    refactor mutant_sampler for greater simplicity

--- mutant_sampler.py
+++ mutant_sampler.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import random
-from typing import Dict, Any, Set, List, Tuple
+from typing import Dict, Tuple
 
 class MutantSampler:
     @staticmethod
@@ -31,186 +30,58 @@ class MutantSampler:
         return df
 
     @staticmethod
-    def drop_duplicates(df: pd.DataFrame) -> pd.DataFrame:
+    def shuffle_dataframe(df: pd.DataFrame) -> pd.DataFrame:
         """
-        Drops duplicate rows from the DataFrame based on 'method_id', 'eventType', and 'mutantId' columns.
+        Shuffles the DataFrame.
 
         Args:
             df: A DataFrame.
 
         Returns:
-            The DataFrame with duplicate rows removed.
+            The DataFrame shuffled.
         """
-        return df.drop_duplicates(subset=['method_id', 'eventType', 'mutantId'])
+        return df.sample(frac=1).reset_index(drop=True)
 
     @staticmethod
-    def populate_mutant_dict(df: pd.DataFrame) -> Dict[str, List[Any]]:
+    def add_row_to_sampled_data(row: pd.Series, sampled_data: pd.DataFrame, event_type_counts: Dict[str, int], k: int) -> Tuple[pd.DataFrame, Dict[str, int]]:
         """
-        Populates a dictionary with event types as keys and a list of mutant IDs as values.
+        Adds a row to the sampled data if it meets the conditions.
 
         Args:
-            df: A DataFrame.
-
-        Returns:
-            A dictionary with event types as keys and corresponding mutant IDs as values.
-        """
-        mutant_dict = {}
-        for _, row in df.iterrows():
-            mutant_dict = MutantSampler.add_to_dict(row, mutant_dict)
-        return mutant_dict
-
-    @staticmethod
-    def add_to_dict(row: pd.Series, mutant_dict: Dict[str, List[Any]]) -> Dict[str, List[Any]]:
-        """
-        Adds a mutant ID to the mutant_dict based on the event type.
-
-        Args:
-            row: A row of the DataFrame.
-            mutant_dict: A dictionary containing event types as keys and mutant IDs as values.
-
-        Returns:
-            The mutant_dict with the new mutant ID added.
-        """
-        if row['eventType'] not in mutant_dict:
-            mutant_dict[row['eventType']] = []
-        mutant_dict[row['eventType']].append(row['mutantId'])
-        return mutant_dict   
-
-    @staticmethod
-    def sort_event_types(mutant_dict: Dict[str, List[Any]]) -> List[str]:
-        """
-        Sorts the event types based on the number of mutant IDs.
-
-        Args:
-            mutant_dict: A dictionary containing event types as keys and mutant IDs as values.
-
-        Returns:
-            A list of event types sorted by the number of mutant IDs.
-        """
-        return sorted(mutant_dict, key=lambda x: len(mutant_dict[x]))
-
-    @staticmethod
-    def get_event_types(mutant_dict: Dict[str, List[Any]]) -> List[str]:
-        """
-        Returns the event types in the order they appear in the dictionary.
-
-        Args:
-            mutant_dict: A dictionary containing event types as keys and mutant IDs as values.
-
-        Returns:
-            A list of event types.
-        """
-        return list(mutant_dict.keys())
-
-    @staticmethod
-    def sample_from_event_type(
-        df: pd.DataFrame,
-        mutant_dict: Dict[str, List[Any]],
-        event_type: str,
-        k: int,
-        sampled_data: pd.DataFrame,
-        sampled_mutants: Set[str]
-    ) -> Tuple[pd.DataFrame, Set[str]]:
-        """
-        Samples k mutant IDs from the given event type and adds them to the sampled data.
-
-        Args:
-            df: A DataFrame.
-            mutant_dict: A dictionary containing event types as keys and mutant IDs as values.
-            event_type: The event type to sample from.
-            k: The number of mutant IDs to sample.
+            row: A row from the DataFrame.
             sampled_data: The DataFrame containing the sampled data.
-            sampled_mutants: A set containing already sampled method IDs.
-
-        Returns:
-            The updated sampled_data and sampled_mutants.
-        """
-        sample_size = min(k, len(mutant_dict[event_type]))
-        sampled_mutant_ids = random.sample(mutant_dict[event_type], sample_size)
-        for mutant_id in sampled_mutant_ids:
-            sampled_data, sampled_mutants = MutantSampler.add_sample_to_data(
-                df, mutant_id, sampled_data, sampled_mutants
-            )
-        return sampled_data, sampled_mutants
-
-    @staticmethod
-    def determine_method_id(df: pd.DataFrame, mutant_id: Any) -> str:
-        """
-        Determines the method ID for a given mutant ID.
-
-        Args:
-            df: A DataFrame.
-            mutant_id: A mutant ID.
-
-        Returns:
-            The method ID corresponding to the mutant ID.
-        """
-        return df[df['mutantId'] == mutant_id]['method_id'].values[0]
-
-    @staticmethod
-    def add_sample_to_data(
-        df: pd.DataFrame,
-        mutant_id: Any,
-        sampled_data: pd.DataFrame,
-        sampled_mutants: Set[str]
-    ) -> Tuple[pd.DataFrame, Set[str]]:
-        """
-        Adds a sample (row) from the DataFrame for the given mutant ID to the sampled data.
-
-        Args:
-            df: A DataFrame.
-            mutant_id: A mutant ID.
-            sampled_data: The DataFrame containing the sampled data.
-            sampled_mutants: A set containing already sampled method IDs.
-
-        Returns:
-            The updated sampled_data and sampled_mutants.
-        """
-        method_id = MutantSampler.determine_method_id(df, mutant_id)
-        if method_id not in sampled_mutants:
-            sampled_mutants.add(method_id)
-            sampled_data = pd.concat([sampled_data, df[df['mutantId'] == mutant_id]])
-        return sampled_data, sampled_mutants
-
-    @staticmethod
-    def get_sampled_data_from_types(
-        df: pd.DataFrame,
-        mutant_dict: Dict[str, List[Any]],
-        sorted_event_types: List[str],
-        k: int
-    ) -> pd.DataFrame:
-        """
-        Generates the sampled data by sampling mutant IDs from each event type.
-
-        Args:
-            df: A DataFrame.
-            mutant_dict: A dictionary containing event types as keys and mutant IDs as values.
-            sorted_event_types: A list of event types sorted by the number of mutant IDs.
+            event_type_counts: A dictionary containing event types as keys and their counts as values.
             k: The number of mutant IDs to sample from each event type.
 
         Returns:
-            The DataFrame containing the sampled data.
+            The sampled_data and event_type_counts updated.
         """
-        sampled_data = pd.DataFrame()
-        sampled_mutants = set()
-        for event_type in sorted_event_types:
-            sampled_data, sampled_mutants = MutantSampler.sample_from_event_type(
-                df, mutant_dict, event_type, k, sampled_data, sampled_mutants
-            )
-        return sampled_data
+        # Check if the method_id of the current row already exists in the sampled data.
+        # If it does, we skip this row to ensure the uniqueness of method_ids in the sampled data.
+        if (sampled_data['method_id'] == row['method_id']).any():
+            return sampled_data, event_type_counts
+
+        # Check if the count of the current row's event type in the sampled data is less than k.
+        # If it is, we add this row to the sampled data and increment the count of this event type.
+        if event_type_counts[row['eventType']] < k:
+            sampled_data = pd.concat([sampled_data, row.to_frame().T])
+            event_type_counts[row['eventType']] += 1
+
+        return sampled_data, event_type_counts
 
     @staticmethod
-    def remove_method_id_column(df: pd.DataFrame) -> pd.DataFrame:
+    def all_event_types_sampled(event_type_counts: Dict[str, int], k: int) -> bool:
         """
-        Removes the 'method_id' column from the DataFrame.
-
+        Checks if all event types have been sampled k times.
         Args:
-            df: A DataFrame.
+
+            event_type_counts: A dictionary containing event types as keys and their counts as values.
+            k: The number of mutant IDs to sample from each event type.
 
         Returns:
-            The DataFrame without the 'method_id' column.
+            True if all event types have been sampled k times, False otherwise.
         """
-        return df.drop(columns=['method_id'])
+        return all(count >= k for count in event_type_counts.values())
 
     @staticmethod
     def get_sampled_data(df: pd.DataFrame, k: int) -> pd.DataFrame:
@@ -224,12 +95,27 @@ class MutantSampler:
         Returns:
             The DataFrame containing the sampled data.
         """
+        # We add a 'method_id' column to the DataFrame to identify unique methods.
         df = MutantSampler.add_method_id_column(df)
-        df = MutantSampler.drop_duplicates(df)
-        mutant_dict = MutantSampler.populate_mutant_dict(df)
-        event_types = MutantSampler.get_event_types(mutant_dict)
-        sampled_data = MutantSampler.get_sampled_data_from_types(
-            df, mutant_dict, event_types, k
-        )
-        # sampled_data = MutantSampler.remove_method_id_column(sampled_data)
-        return sampled_data.sort_values(by='mutantId')
+
+        # Shuffling the DataFrame ensures that our sampling is random.
+        df = MutantSampler.shuffle_dataframe(df)
+
+        # Initialize the counts of all event types to 0.
+        event_type_counts = {et: 0 for et in df['eventType'].unique()}
+        
+        # Initialize an empty DataFrame for storing the sampled data.
+        sampled_data = pd.DataFrame(columns=df.columns)
+
+        # Iterate over each row in the shuffled DataFrame.
+        for _, row in df.iterrows():
+            # Attempt to add the current row to the sampled data.
+            sampled_data, event_type_counts = MutantSampler.add_row_to_sampled_data(row, sampled_data, event_type_counts, k)
+
+            # If we have sampled k mutant IDs for all event types, we can stop sampling.
+            if MutantSampler.all_event_types_sampled(event_type_counts, k):
+                break
+
+        # Sort the sampled data by mutantId before returning.
+        sampled_data = sampled_data.sort_values(by='mutantId')
+        return sampled_data
